b2b1c784ec91d8bf9fe21ae1fb5f94ffabd2e7c4
Argyrios <310325170+argyrios-dev@users.noreply.github.com>
2026-08-26 19:52:50 +0200

    Refresh offline cache for v1.1.2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='toolynx-v1.1.1';
-const ASSETS=['./','./index.html','./styles.css','./topbar-fix.css','./app.js','./manifest.webmanifest','./assets/icon.svg'];
+const CACHE='toolynx-v1.1.2';
+const ASSETS=['./','./index.html','./styles.css','./topbar-fix.css','./app.js','./unlimited-generators.js','./manifest.webmanifest','./assets/icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
